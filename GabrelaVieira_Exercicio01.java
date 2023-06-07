cb260c55853e8a6d45094926f51d2a34632861bf
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-06-07 17:55:03 -0300

    Update GabrelaVieira_Exercicio01.java

--- GabrelaVieira_Exercicio01.java
+++ GabrelaVieira_Exercicio01.java
@@ -5,8 +5,28 @@ Data: 07/06/2023
 */public class GabrelaVieira_Exercicio01 {
     public static void main (String [] args) {
 
-        /* CRIA UMA INSTÂNCIA DA CLASSE ALIMENTO */
-        Alimento comer = new Alimento 
+       /* ACESANDO OS ATRIBUTOS DA COMIDA*/
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        
+        Alimento comer = new Alimento ()
+
     }
-    
-}
+}   
+class Alimento {
+    /*CRIAÇÃO DE ATRIBUTOS */
+        String validade, gosto, textura, aroma, forma, cor;
+        double tamanho;
+
+     
+}       
+
+        
+  
