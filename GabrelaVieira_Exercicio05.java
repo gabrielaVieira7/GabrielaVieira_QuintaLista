fcab9dad31568c20bddc0782fc097c4f4145f698
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-06-12 14:34:58 -0300

    Update GabrelaVieira_Exercicio05.java

--- GabrelaVieira_Exercicio05.java
+++ GabrelaVieira_Exercicio05.java
@@ -6,7 +6,149 @@ Data: 07/06/2023
 public class GabrelaVieira_Exercicio05 {
     public void main (String [] args) {
 
-        /*CRIA  */
+        public class Exemplo {
+    private int numero1;
+    private int numero2;
+    private String texto;
+    private double valor;
+    private boolean status;
+    private char caractere;
+    private int[] numeros;
+    private String[] nomes;
+    private Object objeto;
+    private static int contador;
+
+    public Exemplo(int numero1, int numero2, String texto, double valor, boolean status, char caractere, int[] numeros, String[] nomes, Object objeto) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.texto = texto;
+        this.valor = valor;
+        this.status = status;
+        this.caractere = caractere;
+        this.numeros = numeros;
+        this.nomes = nomes;
+        this.objeto = objeto;
+        contador++;
+    }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public char getCaractere() {
+        return caractere;
+    }
+
+    public void setCaractere(char caractere) {
+        this.caractere = caractere;
+    }
+
+    public int[] getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(int[] numeros) {
+        this.numeros = numeros;
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    public Object getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Object objeto) {
+        this.objeto = objeto;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public void metodo1() {
+        // Implementação do método 1
+    }
+
+    public void metodo2() {
+        // Implementação do método 2
+    }
+
+    public void metodo3() {
+        // Implementação do método 3
+    }
+
+    public void metodo4() {
+        // Implementação do método 4
+    }
+
+    public void metodo5() {
+        // Implementação do método 5
+    }
+
+    public void metodo6() {
+        // Implementação do método 6
+    }
+
+    public void metodo7() {
+        // Implementação do método 7
+    }
+
+    public void metodo8() {
+        // Implementação do método 8
+    }
+
+    public void metodo9() {
+        // Implementação do método 9
+    }
+
+    public void metodo10() {
+        // Implementação do método 10
+    }
+}
+
+       
     }
 
     
