44760c2f2af8b553f1e9e82d7dfa918fb8e11534
GabrielaVieira7 <131814122+gabrielaVieira7@users.noreply.github.com>
2023-06-12 17:57:21 -0300

    Update GabrelaVieira_Exercicio02.java

--- GabrelaVieira_Exercicio02.java
+++ GabrelaVieira_Exercicio02.java
@@ -3,66 +3,34 @@ Autora: Gabriela
 Data: 07/06/2023
 */
 
-public class GabrelaVieira_Exercicio02 {
+public class GabrielaVieira_Exercicio01 {
     public static void main (String [] args) {
-        
-           public class Cinto {
-    private String material;
-    private String cor;
-    private int tamanho;
-    private boolean fivela;
-
-    // Métodos da classe
-    public void apertarCinto() {
-        System.out.println("Cinto apertado.");
     }
+    
+    class Cinto{
+       void apertar(){
+        System.out.println("Esta apertando");
+       }
 
-    public void soltarCinto() {
-        System.out.println("Cinto solto.");
-    }
+       void afivelar(){
+        System.out.println("Esta apertando");
+       }
 
-    public void ajustarTamanho(int novoTamanho) {
-        this.tamanho = novoTamanho;
-        System.out.println("Tamanho do cinto ajustado para: " + tamanho);
-    }
+       void apertar(){
+        System.out.println("Esta apertando");
+       }
 
-    public void trocarFivela(String novaFivela) {
-        this.fivela = true;
-        System.out.println("Fivela do cinto trocada para: " + novaFivela);
-    }
+       void apertar(){
+        System.out.println("Esta apertando");
+       }
 
-    public void exibirDetalhes() {
-        System.out.println("Material: " + material);
-        System.out.println("Cor: " + cor);
-        System.out.println("Tamanho: " + tamanho);
-        System.out.println("Possui fivela: " + (fivela ? "Sim" : "Não"));
-    }
+       void apertar(){
+        System.out.println("Esta apertando");
+       }            
 
-    // Getters e Setters
-    public String getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public int getTamanho() {
-        return tamanho;
-    }
-
-    public boolean hasFivela() {
-        return fivela;
-    }
-
+        }
+       }
+    
     public void setFivela(boolean fivela) {
         this.fivela = fivela;
     }
